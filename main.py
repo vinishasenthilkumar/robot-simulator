64d4e44a84c6c59d64ae9c210ab6e60a1a8c362f
vinishasenthilkumar <113600023+vinishasenthilkumar@users.noreply.github.com>
2025-07-05 17:28:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 # main.py
 
-from robot import Terrain
+from robot import RobotSimulator
 
 def main():
-    terrain = Terrain()
+    simulator = RobotSimulator()
 
-    terrain.create_robot("R1")
-    terrain.create_robot("R2")
+    simulator.add_robot("R1")
+    simulator.add_robot("R2")
 
-    terrain.move_robot("R1", "E3")
-    print(f"R1: {terrain.get_robot_position('R1')}")  
+    simulator.issue_command("R1", "E3")
+    print(f"R1: {simulator.get_robot_current_position('R1')}")
 
-    terrain.move_robot("R2", "E5")  
-    print(f"R2: {terrain.get_robot_position('R2')}")  
+    simulator.issue_command("R2", "E5")
+    print(f"R2: {simulator.get_robot_current_position('R2')}")
 
-    terrain.move_robot("R1", "N2")
-    print(f"R1: {terrain.get_robot_position('R1')}")  
+    simulator.issue_command("R1", "N2")
+    print(f"R1: {simulator.get_robot_current_position('R1')}")
 
-    terrain.move_robot("R2", "E1")
-    print(f"R2: {terrain.get_robot_position('R2')}")  
+    simulator.issue_command("R2", "E1")
+    print(f"R2: {simulator.get_robot_current_position('R2')}")
 
 if __name__ == "__main__":
     main()
